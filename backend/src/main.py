9b0d65953fd795f1afc596b259006c1d31b5ccb1
balaji1359 <nagisetty.balaji@gmail.com>
2022-07-10 19:06:37 +0200

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,6 +4,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://18.221.40.209:3000",
+    "http://localhost:8080",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
