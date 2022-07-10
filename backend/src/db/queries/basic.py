fa6084654b7d8140d73bfec4704656961e078a12
balaji1359 <nagisetty.balaji@gmail.com>
2022-07-10 18:25:15 +0200

    Update basic.py

--- backend/src/db/queries/basic.py
+++ backend/src/db/queries/basic.py
@@ -6,12 +6,13 @@ driver = GraphDatabase.driver(uri, auth=("neo4j", "Balu@2481358"))
 def get_friends_of(tx, name):
     friends = []
     result = tx.run("""match p=(a:Person)-[]->(b:Book)
-unwind nodes(p) as n unwind relationships(p) as r
-with collect( distinct {id: ID( n), 
-label:labels(n), name: n.FirstNname, 
-gender:n.Gender, title:n.title}) as nl, 
-collect( distinct {source: ID(startnode(r)), target: ID(endnode(r))}) as rl
-RETURN {nodes: nl, links: rl}""")
+                        unwind nodes(p) as n unwind relationships(p) as r
+                        with collect( distinct {id: ID( n), 
+                        label:labels(n), name: n.FirstNname, 
+                        gender:n.Gender, title:n.title,
+                        url:n.img_url}) as nl, 
+                        collect( distinct {source: ID(startnode(r)), target: ID(endnode(r))}) as rl
+                        RETURN {nodes: nl, links: rl}""")
     for record in result:
         friends.append(record.data())
     return friends
